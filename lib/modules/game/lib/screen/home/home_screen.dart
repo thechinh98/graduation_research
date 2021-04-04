f179a81e368c6b21cb565dab12dc3793e4b61a13
thechinh98 <chinh1998@gmail.com>
2021-04-04 16:05:15 +0700

    Update UI

--- lib/modules/game/lib/screen/home/home_screen.dart
+++ lib/modules/game/lib/screen/home/home_screen.dart
@@ -23,7 +23,7 @@ class _HomeScreenState extends State<HomeScreen> {
         child: ElevatedButton(
           child: Text('Quiz Game'),
           onPressed: () {
-            final topicId = '4571048042299392';
+            final topicId = '4536143782608896';
             Navigator.of(context).push(
               MaterialPageRoute(builder: (_) => StudyScreen(topicId)),
             );
